c2131a9955f6ae6af035790208dd123c7502bc20
Francia Csaba <h157595@stud.u-szeged.hu>
2023-10-21 20:06:44 +0200

    New race form

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,13 +1,63 @@
-<template>
-  <h1>Hello</h1>
-</template>
-
 <script>
-export default {
+import Layout from "../Shared/Layout.vue";
 
+export default {
+	layout: Layout,
+	data: () => ({
+		name: "",
+		year: 2023,
+		nameRules: [
+			value => {
+				if (value) return true
+				return 'A nevet meg kell adni!'
+			},
+		],
+		yearRules: [
+			value => {
+				if (value) return true
+				return 'Az évet meg kell adni!'
+			},
+		],
+	})
 }
 </script>
 
-<style>
+<template>
+	<div class="wrapper flex justify-space-between flex-wrap">
 
+		<h2 class="text-overline">Versenyek</h2>
+		<v-dialog width="500">
+			<template v-slot:activator="{ props }">
+				<v-btn color="success" v-bind="props"> <v-icon left>mdi-plus</v-icon> Új verseny felvétele </v-btn>
+			</template>
+
+			<template v-slot:default>
+				<v-card class="pa-5" title="Új verseny felvétele">
+					<v-form @submit.prevent>
+						<v-text-field
+							v-model="name"
+							:rules="nameRules"
+							required
+							label="Név"></v-text-field>
+						<v-text-field
+							v-model.number="year"
+							:rules="yearRules"
+							required
+							label="Év"></v-text-field>
+
+					</v-form>
+
+					<v-btn type="submit" color="success" block outline class="mt-2">
+						Új verseny</v-btn>
+				</v-card>
+			</template>
+		</v-dialog>
+	</div>
+</template>
+
+<style scoped>
+.wrapper {
+	display: flex;
+	gap: 1em;
+}
 </style>
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<template>
+	<section class="pa-5 header">
+		<v-icon size="1.5em" color="black">mdi-flag-checkered</v-icon>
+		<h1>TrackMaster</h1>
+	</section>
+
+	<section class="pa-6">
+		<div class="main mx-auto">
+			<slot />
+		</div>
+	</section>
+</template>
+
+<style scoped>
+.main{
+	max-width: 1000px;
+}
+
+.header{
+	display: flex;
+	gap: 0.2em;
+	background: rgb(163,188,221);
+	background: radial-gradient(circle, rgba(163,188,221,1) 19%, rgba(150,185,249,1) 53%, rgba(111,137,191,1) 100%);
+}
+</style>
