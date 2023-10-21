951204c24de8f17a94e90e220ab034d497855900
Francia Csaba <h157595@stud.u-szeged.hu>
2023-10-21 18:19:33 +0200

    Added Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,16 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+	components,
+	directives,
+  })
+
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
@@ -9,6 +19,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+	  .use(vuetify)
       .mount(el)
   },
 })
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from "@vitejs/plugin-vue";
 import laravel from 'laravel-vite-plugin';
+import path from 'path'
 
 export default defineConfig({
-    plugins: [
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		},
+	},
+	plugins: [
 		vue(),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+		laravel({
+			input: ['resources/css/app.css', 'resources/js/app.js'],
+			refresh: true,
+		}),
+	],
 });
